62f06fdb045a82289986a0b5fc1fe18c369df47a
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-09-28 09:48:42 +0000

    Deploying to gh-pages from @ victoria2588/goit-react-hw-08-phonebook@a1410aecc3514f2195b947c9877f7fe2f2db121b 🚀

---
+++ static/js/266.91d5a1e8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Welcome to PHONEBOOK!"})})}}}]);
+//# sourceMappingURL=266.91d5a1e8.chunk.js.map
